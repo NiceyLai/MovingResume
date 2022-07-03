598b17b1f86cc9a7d0a0ca28e00acadcc49093f0
nicey <1919325005@qq.com>
2022-07-03 13:53:39 +0800

    第二次配置环境

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -178,7 +178,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60186" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60648" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -354,4 +354,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=main.1f19ae8e.js.map
+//# sourceMappingURL=dist/main.1f19ae8e.js.map
